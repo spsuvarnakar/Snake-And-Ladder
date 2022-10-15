3d6ba0a9a46ce3c51cdfb8318ce64c21d43ba565
Suvarnkar Prabhu <41757793+spsuvarnakar@users.noreply.github.com>
2022-10-15 17:45:00 +0530

    Add files via upload

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,7 +7,21 @@ public class SnakeAndLadder {
 	public static void main(String args[]) {
 //WELCOME MESSAGE FOR SNAKE & LADDDER PROGRAM
 		System.out.println("Welcome To Snake And Ladder Simulater program");
-
+		
+		int position=0;
+		int count=0;
+		
+		System.out.println("Start position="+position);
+		
+		Random r=new Random();
+		
+		int dice=r.nextInt(6)+1;
+		
+		System.out.println("Dice outcome is="+dice);
+		
+		count++;
+		
+		System.err.println("Total count="+count);
 	}
 
 

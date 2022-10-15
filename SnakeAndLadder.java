c0a4949624e778dcd65761516cc47ad94d04eb9a
Suvarnkar Prabhu <41757793+spsuvarnakar@users.noreply.github.com>
2022-10-15 17:43:54 +0530

    Welcome Message Added

---
+++ SnakeAndLadder.java
@@ -0,0 +1,15 @@
+package xyz;
+
+import java.util.Random;
+
+public class SnakeAndLadder {
+
+	public static void main(String args[]) {
+//WELCOME MESSAGE FOR SNAKE & LADDDER PROGRAM
+		System.out.println("Welcome To Snake And Ladder Simulater program");
+
+	}
+
+
+		
+	}
